f2baf89d837570762fed90ff2e54dc7419ad74c2
António Sérgio Furtado <17sergio.furtado@gmail.com>
2025-06-17 21:47:57 -0100

    finish project water quality

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 class WaterQualityEvaluator:
     def __init__(self, ph_range=(6.5, 8.5), turbidity_threshold=1.0):
         self.ph_range = ph_range
@@ -7,3 +9,14 @@ class WaterQualityEvaluator:
         """
         Determine if a row of water data is safe.
         """
+        column_metric = row.name
+
+        # False if value is missing on pH or turbidity
+        if row.isna().any():
+            return False
+
+        if column_metric == "pH":
+            return ((row < 6.5) | (row > 8.5)).any()
+
+        if column_metric == "turbidity":
+            return (row < 1).any()
